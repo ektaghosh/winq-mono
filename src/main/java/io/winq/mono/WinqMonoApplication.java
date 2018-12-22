7a5f30810eff20d95c17a2c46b55b7de11021d50
Ekta Ghosh <ektaghosh2017@gmail.com>
2018-12-23 01:24:24 +0530

    Initial Commit

---
+++ src/main/java/io/winq/mono/WinqMonoApplication.java
@@ -0,0 +1,14 @@
+package io.winq.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinqMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinqMonoApplication.class, args);
+	}
+
+}
+
